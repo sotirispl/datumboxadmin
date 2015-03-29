9b2355bab92c400ad58241902edbf4ee6f4a9042
Sotiris Poulias <str.poulias@gmail.com>
2015-03-29 16:31:39 +0300

    Initialized Administrator Panel - Primefaces

--- src/main/java/com/sotiris/adminpanel/beans/AnalysisBean.java
+++ src/main/java/com/sotiris/adminpanel/beans/AnalysisBean.java
@@ -1,10 +1,14 @@
 package com.sotiris.adminpanel.beans;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -20,6 +24,14 @@ import com.sotiris.adminpanel.utils.MongoConfig;
 public class AnalysisBean {
 
 	private List<Analysis> analysis;
+	private MongoOperations mongoOperation;
+	
+	private SelectItem[] modesOptions;
+	private SelectItem[] uidsOptions;
+	
+	public AnalysisBean() {
+		analysis = new ArrayList<Analysis>();
+	}
 	
 	public List<Analysis> getAnalysis() {
 		return analysis;
@@ -28,19 +40,59 @@ public class AnalysisBean {
 	public void setAnalysis(List<Analysis> analysis) {
 		this.analysis = analysis;
 	}
-
-	public void getAllAnalysis() {
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
-		MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
-		DBCollection analysisDB = mongoOperation.getCollection("analysis");
-		DBCursor itr = analysisDB.find();
-		analysis = new ArrayList<Analysis>();
-		while(itr.hasNext()) {
-			Analysis obj = (Analysis) itr.next();
-			System.out.println(obj.getText());
-			analysis.add(obj);
-		}
+	
+	public SelectItem[] getModesOptions() {
+		return modesOptions;
+	}
+	public void setModesOptions(SelectItem[] modesOptions) {
+		this.modesOptions = modesOptions;
 	}
 	
+	public SelectItem[] getUidsOptions() {
+		return uidsOptions;
+	}
+	public void setUidsOptions(SelectItem[] uidsOptions) {
+		this.uidsOptions = uidsOptions;
+	}
 	
+	private SelectItem[] createFilterOptions(List<String> data)  {
+        SelectItem[] options = new SelectItem[data.size() + 1];
+
+        options[0] = new SelectItem("", "Select");
+        
+        Iterator<String> itr = data.iterator();
+        int i = 1;
+        while(itr.hasNext()) {
+        	String tmp = itr.next();
+        	options[i] = new SelectItem(tmp, tmp);
+        	i++;
+        }
+
+        return options;
+    }
+	
+	@PostConstruct
+	public void getAllAnalysis() {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
+		mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
+		analysis = mongoOperation.findAll(Analysis.class);
+		
+		ArrayList<String> modeList = new ArrayList<String>();
+		ArrayList<String> userList = new ArrayList<String>();
+		
+		for(Analysis obj : analysis) {
+
+			if(!userList.contains(obj.getUserid())) {
+				userList.add(obj.getUserid());
+			}
+		}
+		for(Analysis obj : analysis) {
+			if(!modeList.contains(obj.getMode())) {
+				modeList.add(obj.getMode());
+			}
+		}
+
+		modesOptions = createFilterOptions(modeList);
+		uidsOptions = createFilterOptions(userList);
+	}
 }
